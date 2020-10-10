76773c3b66068d9e3738e1a797b808fac3db50b8
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-10 19:44:42 +0530

    [Arijit]Added : Created Queue And Performed Enqueue

---
+++ src/test/java/com/cg/QueueTest.java
@@ -0,0 +1,21 @@
+package com.cg;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class QueueTest {
+      @Test
+      public void enQueueTest() {
+    	  Queue<Integer> queue = new Queue<>();
+    	  Node<Integer> firstNode = new Node<>(70);
+  		Node<Integer> secondNode = new Node<>(30);
+  		Node<Integer> thirdNode = new Node<>(65);
+  		queue.enQueue(firstNode);
+  		queue.enQueue(secondNode);
+  		queue.enQueue(thirdNode);
+  		queue.printQueue();
+  		int size = queue.size();
+  		Assert.assertEquals(3, size);
+      }
+}
